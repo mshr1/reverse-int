420a790a596ad88ad1e21324cda542395ca0460a
Sergey <sergey@example.com>
2022-06-24 18:54:23 +0300

    docs: make tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = Math.abs(-n);
+    }
+    n = n.toString();
+    newNum = n.split("");
+    newNum.reverse();
+    n = newNum.join("");
+    n = Number(n);
+
+    return n;
+};
